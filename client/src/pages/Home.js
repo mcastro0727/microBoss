709d4f8efa685e7ae86f9399849caab69f69561f
Melissa Castro <melissa.anne.castro@gmail.com>
2019-12-05 13:13:38 -0500

    resolved more conflicts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,48 +10,6 @@ import Signup from "../components/Signup";
 import "../index.css";
 import EpicMenu from "../components/EpicMenu/EpicMenu";
 class Home extends Component {
-<<<<<<< HEAD
-
-    // STATE
-
-    // loggedIn
-    // !loggedIn
-    // signUp ????
-
-
-    render() {
-        return(
-            <React.Fragment>
-            <Navbar bg="light" variant="light">
-    <Navbar.Brand href="/" style={{ fontWeight: "bolder", fontStyle: 'italic' }}>microBoss</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="/profile">Profile</Nav.Link>
-      
-      <Nav.Link href="#contact">Contact Us</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-primary">Search</Button>
-    </Form>
-  </Navbar>
-
-            <h1>Home</h1>
-            <h3>The miniature project boss in your pocket</h3>
-            <ul>
-                <Login />
-                <br/>
-                <Signup />
-            </ul>
-
-         </React.Fragment>   
-                
-            
-        )
-    }
-
-
-
-=======
   // STATE
 
   // loggedIn
@@ -78,7 +36,6 @@ class Home extends Component {
       </React.Fragment>
     );
   }
->>>>>>> 57f36479022a99dd4a80993a58b32f0d106fdf8a
 }
 
 export default Home;
